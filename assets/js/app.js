6398cbb24d964bac98fcd8343c6edcc6aed082b8
ALEXX4891 <alexx4891@mail.ru>
2024-09-04 23:18:26 +0500

    добавит адаптив странице "Сотрудничество"

--- assets/js/app.js
+++ assets/js/app.js
@@ -722,52 +722,52 @@ if (checkbox) {
 }
 // -------------------------------------------- end checkbox-lable: ---------------------------------------------
 // -------------------------------------------- start slider: ---------------------------------------------
-// new Swiper(".news-slider_swiper", {
-//   // Optional parameters
-//   direction: "horizontal",
-//   // loop: true,
-//   // allowTouchMove: true,
-//   slidesPerView: 3.2, // сколько слайдов показывать, можно дробно
-//   // slidesPerView: 'auto', // сколько слайдов показывать, можно дробно
-//   // slidersPerGroup: 3, // сколько слайдов в группе
-//   centeredSlides: true, //выравнивание слайдов по центру
-//   initialSlide: 1, //начальный слайд (c нуля)
+new Swiper(".news-slider_swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  // loop: true,
+  // allowTouchMove: true,
+  slidesPerView: 3.2, // сколько слайдов показывать, можно дробно
+  // slidesPerView: 'auto', // сколько слайдов показывать, можно дробно
+  // slidersPerGroup: 3, // сколько слайдов в группе
+  centeredSlides: true, //выравнивание слайдов по центру
+  initialSlide: 1, //начальный слайд (c нуля)
 
-//   spaceBetween: 40,
-//   // slideToClickedSlide: true, //перелистывание слайдов по клику
-//   // grabCursor: true, //меняет курсор при наведении на руку
-//   watchOverflow: true, //отключает слайдер если все слайды входят в область видимости
+  spaceBetween: 40,
+  // slideToClickedSlide: true, //перелистывание слайдов по клику
+  // grabCursor: true, //меняет курсор при наведении на руку
+  watchOverflow: true, //отключает слайдер если все слайды входят в область видимости
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   // mousewheel: { //перелистывание слайдов по мышке
-//   //   sensitivity: 1,
-//   //   eventsTarget: ".news__slider",
-//   // },
-//   // keyboard: { //перелистывание слайдов по нажатию клавиш
-//   //   enabled: true,
-//   //   onlyInViewport: true,
-//   //   // pageUpDown: true,
-//   // },
-//   breakpoints: {
-//     0: {
-//       slidesPerView: 1,
-//     },
-//     500: {
-//       slidesPerView: 2,
-//     },
-//     800: {
-//       slidesPerView: 3.35,
-//     },
-//   },
-// });
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // mousewheel: { //перелистывание слайдов по мышке
+  //   sensitivity: 1,
+  //   eventsTarget: ".news__slider",
+  // },
+  // keyboard: { //перелистывание слайдов по нажатию клавиш
+  //   enabled: true,
+  //   onlyInViewport: true,
+  //   // pageUpDown: true,
+  // },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    800: {
+      slidesPerView: 3.35,
+    },
+  },
+});
 
 new Swiper(".building_swiper", {
   // Optional parameters
@@ -775,7 +775,7 @@ new Swiper(".building_swiper", {
   // loop: true,
   // allowTouchMove: true,
   // slidesPerView: auto, // сколько слайдов показывать, можно дробно
-  slidesPerView: "auto", // сколько слайдов показывать, можно дробно
+  slidesPerView: 1, // сколько слайдов показывать, можно дробно
   // slidersPerGroup: 3, // сколько слайдов в группе
   // centeredSlides: true, //выравнивание слайдов по центру
   // initialSlide: 0, //начальный слайд (c нуля)
