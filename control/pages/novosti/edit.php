39e33bac1007b16dfffa271f2e1b5382273c8383
il115 <web@ww.net.ru>
2024-09-18 15:52:43 +0500

    добавит верстку страницы редактирования новости

--- control/pages/novosti/edit.php
+++ control/pages/novosti/edit.php
@@ -5,115 +5,97 @@ $keywords = '';
 include $_SERVER["DOCUMENT_ROOT"] . '/control/includes/head.php';
 ?>
 
-<body class="body control__body">
+<body class="body control__body control__body_news">
 
   <?
   include $_SERVER["DOCUMENT_ROOT"] . '/control/includes/header.php';
   ?>
 
   <main class="main control">
-    <section class="section control__section">
+    <section class="section control__section edit-page">
       <div class="container control__container">
-        <div class="control__head choice__form">
-
-          <div class="choice__select select select-frame">
-            <div class="select__wrapper">
-              <span class="select__placeholder">
-                Тип
+        <a href="/control/pages/novosti/index.php" class="control__back">
+          <svg width="9" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 13.4131L1 7.41309L8 1.41309" stroke="#999999" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <span class="control__back-text">
+            Назад
+          </span>
+        </a>
+        <h1 class="control__title">
+          Создать новость
+        </h1>
+        <div class="control__head control__form">
+          <div class="control__form-top-wrap">
+            <label for="title" class="control__label control__label_title">
+              <span>
+                Заголовок
               </span>
-              <p class="select__text"></p>
-            </div>
-            <span class="select__arrow">
-              <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1.3335 1L7.54039 7L13.3335 1" stroke="#363636" stroke-width="1.5" stroke-linejoin="round" />
-              </svg>
-            </span>
-            <ul class="select__list">
-              <li class="select__item" data-id="1">Опубликованы</li>
-              <li class="select__item" data-id="2">Черновики</li>
-              <li class="select__item" data-id="3">Все</li>
-            </ul>
+              <input id="title" class="control__input control__input_title" name="title" type="text" placeholder="Заголовок">
+            </label>
+  
+            <label for="date" class="control__label control__label_date">
+              <span>
+                Дата
+              </span>
+              <input id="date" class="control__input control__input_date" type="date" name="date">
+            </label>
+  
+            <label for="photo" class="control__label control__label_photo">
+              <span>
+                Обложка (1712х563 px)
+              </span>
+              <input id="photo" class="control__input control__input_photo" type="file" name="photo" hidden>
+              <div class="control__photo-wrap">
+                <img src="/assets/img/blank.jpg" class="control__photo" alt="Обложка">
+              </div>
+              <button class="control__btn control__btn_photo">
+                Загрузить фото
+              </button>
+            </label>
           </div>
 
-          <a class="control__btn btn btn_green" href="/control/pages/novosti/edit.php">
-            Создать новость
-          </a>
+
+          <label for="text" class="control__label control__label_text">
+            <span>
+              Текст новости
+            </span>
+            <textarea id="text" class="control__input control__input_text" name="description" placeholder="Текст"></textarea>
+          </label>
+
+          <div class="control__gallery-wrap">
+            <span>
+              Галерея
+            </span>
+
+            <label for="gallery" class="control__label control__label_gallery">
+              <input id="gallery" class="control__input control__input_gallery" type="file" name="gallery" hidden>
+              <div class="control__gallery-wrap">
+                <img src="/assets/img/blank-gallery.jpg" class="control__gallery" alt="Обложка">
+              </div>
+              <button class="control__btn control__btn_gallery">
+                Загрузить фото
+              </button>
+            </label>
+          </div>
+
+          <label for="check" class="control__label control__label_check">
+            <input id="check" class="control__check control__input_check" type="checkbox" name="check">
+            <span>
+              Опубликовать
+            </span>
+          </label>
+
+          <div class="control__btn-wrap">
+            <button class="control__input control__input_submit btn btn_green" type="submit">Сохранить</button>
+            <button class="control__btn control__btn_cancel btn btn_white">Отмена</button>
+          </div>
+
         </div>
       </div>
     </section>
 
-    <section class="section control__section content">
 
-      <div class="container control__container control__content">
-
-        <ul class="control__content-list">
-
-        <?
-            $resultNews = mysqli_query($db, "SELECT * FROM news ORDER BY date ASC");
-            $news = mysqli_fetch_array($resultNews);
-
-            if (mysqli_num_rows($resultNews) > 0) {
-              do {
-
-                // echo $news['date'];
-                // $date = $date ?? 'Опубликовать';
-                if(strtotime($news['date']) == 0) {
-                  $date = 'ОПУБЛИКОВАТЬ';
-                } else {  
-                  $date = date("d.m.Y", strtotime($news['date']));
-                }
-
-
-                echo "
-                <li class='control__content-item control__card card'>
-                <div class='card__head'>
-                  <span class='card__head-title'>
-                    Дата публикации:
-                  </span>
-                  <span class='card__head-date'>
-                  {$date}
-                  </span>
-                </div>
-    
-                <h2 class='card__title'>
-                {$news['title']}
-                </h2>
-    
-                <div class='card__btn-block'>
-                  <button class='card__btn card__btn_publish'>
-                    <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path d='M3 13.3906C6.6 5.39062 17.4 5.39062 21 13.3906' stroke='#999999' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
-                      <path d='M12 17.3906C11.606 17.3906 11.2159 17.313 10.8519 17.1623C10.488 17.0115 10.1573 16.7905 9.87868 16.5119C9.6001 16.2334 9.37913 15.9027 9.22836 15.5387C9.0776 15.1747 9 14.7846 9 14.3906C9 13.9967 9.0776 13.6066 9.22836 13.2426C9.37913 12.8786 9.6001 12.5479 9.87868 12.2693C10.1573 11.9907 10.488 11.7698 10.8519 11.619C11.2159 11.4682 11.606 11.3906 12 11.3906C12.7956 11.3906 13.5587 11.7067 14.1213 12.2693C14.6839 12.8319 15 13.595 15 14.3906C15 15.1863 14.6839 15.9493 14.1213 16.5119C13.5587 17.0746 12.7956 17.3906 12 17.3906Z' stroke='#999999' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
-                    </svg>
-    
-                  </button>
-    
-                  <button class='card__btn card__btn_edit'>
-                    <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path d='M7 7.39062H6C5.46957 7.39063 4.96086 7.60134 4.58579 7.97641C4.21071 8.35148 4 8.86019 4 9.39062V18.3906C4 18.9211 4.21071 19.4298 4.58579 19.8048C4.96086 20.1799 5.46957 20.3906 6 20.3906H15C15.5304 20.3906 16.0391 20.1799 16.4142 19.8048C16.7893 19.4298 17 18.9211 17 18.3906V17.3906' stroke='#999999' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
-                      <path d='M16 5.39073L19 8.39073M20.385 6.97573C20.7788 6.58189 21.0001 6.04772 21.0001 5.49073C21.0001 4.93375 20.7788 4.39958 20.385 4.00573C19.9912 3.61189 19.457 3.39063 18.9 3.39062C18.343 3.39062 17.8088 3.61189 17.415 4.00573L9 12.3907V15.3907H12L20.385 6.97573Z' stroke='#999999' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
-                    </svg>
-    
-                  </button>
-    
-                  <button class='card__btn card__btn_delete'>
-                    <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                      <path d='M5.89355 6.69043L6.8107 20.5319H16.8475L18.1066 6.69043H5.89355Z' stroke='#999999' stroke-width='1.5' stroke-linejoin='round' />
-                      <path d='M11.7934 10.7615V16.0538M3.85791 6.69043H20.142' stroke='#999999' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
-                      <path d='M8.74316 6.69066L10.0821 4.24805H13.9447L15.2568 6.69066H8.74316Z' stroke='#999999' stroke-width='1.5' stroke-linejoin='round' />
-                    </svg>
-    
-                  </button>
-                </div>
-              </li>
-                ";
-              } while ($news = mysqli_fetch_array($resultNews));
-            }
-                ?>
-        </ul>
-
-      </div>
-    </section>
   </main>
 
   <?
