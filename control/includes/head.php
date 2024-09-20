15b31ed2b59f4e10deea9e61e378571fed7f2da7
il115 <web@ww.net.ru>
2024-09-20 16:54:16 +0500

    исправит баг

--- control/includes/head.php
+++ control/includes/head.php
@@ -5,7 +5,7 @@ include $_SERVER["DOCUMENT_ROOT"] . '/backend/f.php';
 if(!$_SESSION['user_id'] AND !$_SESSION['access']){
   // die($_SESSION['access']);
   // echo '<meta http-equiv="refresh" content="0; URL=\'/control/login.php\'" />'; 
-  header('Location: /control/login.php');
+  header('Location: ' . $_SERVER["DOCUMENT_ROOT"] . ' /control/login.php');
   exit();
 }
 
