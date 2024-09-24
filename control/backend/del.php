3c83ba94572940a7091265b1b52d0d7828ac998a
ALEXX4891 <alexx4891@mail.ru>
2024-09-24 19:17:05 +0500

    добавит удаление фотографий при удалении новосит или акции

--- control/backend/del.php
+++ control/backend/del.php
@@ -1,4 +1,5 @@
 <?
+error_reporting(E_ALL);
 
 $inputData = file_get_contents('php://input');
 
@@ -9,14 +10,10 @@ if (!$inputData) {
 include $_SERVER["DOCUMENT_ROOT"] . '/backend/db.php';
 include $_SERVER["DOCUMENT_ROOT"] . '/backend/f.php';
 
-
 if ($inputData) {
   $data = json_decode($inputData, true);
   $arrKeys = array_keys($data);
   $arrData = [];
-
-// var_dump(value: $arrKeys);
-
 
   foreach ($arrKeys as $key) {
     if ($key == 'function') {
@@ -36,8 +33,36 @@ if ($inputData) {
     }
   }
 
-
 if ($function == 'del') {
+
+  $query = sprintf(
+    "SELECT * FROM `$table` WHERE `id` = '%s'",
+    mysqli_real_escape_string($db, $id)
+  );
+
+  $result = mysqli_query($db, $query);  
+  $row = mysqli_fetch_array($result);
+  
+  $photoFileArr = [
+    'photo',
+    'image-1',
+    'image-2',
+    'image-3',
+    'image-4',
+    'image-5',
+    'image-6',
+    'image-7',
+    'image-8',
+    'image-9',
+    'image-10'
+  ];
+  
+  foreach ($photoFileArr as $value) {
+    if ($row[$value]) {
+      unlink($_SERVER['DOCUMENT_ROOT'].'/assets/img/'.$row[$value]);
+    } 
+  }
+
     $sql = sprintf(
       "DELETE FROM `$table` WHERE `id` = '%s'",
       mysqli_real_escape_string($db, $id)
