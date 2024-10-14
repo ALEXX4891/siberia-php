ab1664159794794bfadd7a9206a5d938c4249511
il115 <web@ww.net.ru>
2024-10-14 16:35:30 +0500

    изменит картинку и сортировку акций

--- pages/akcii/index.php
+++ pages/akcii/index.php
@@ -34,7 +34,9 @@ include $_SERVER["DOCUMENT_ROOT"] . '/includes/head.php';
           <?
           // include $_SERVER["DOCUMENT_ROOT"] . '/backend/f.php';
           // $result = mysqli_query($db, "SELECT * FROM apartments WHERE id = " . $_GET['id']);
-          $result = mysqli_query($db, "SELECT * FROM events WHERE status = 1");
+          // $result = mysqli_query($db, "SELECT * FROM events WHERE status = 1");
+          $result = mysqli_query($db, "SELECT * FROM events WHERE status = 1 ORDER BY DATE DESC");
+
 
           $row = mysqli_fetch_array($result);
 
